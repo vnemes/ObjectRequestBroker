c590092b0e0e35d84e92da1e995b5bda3665e7cc
vendettavn <vladnemes5@gmail.com>
2018-05-05 17:04:59 +0300

    Added a test to ensure that the two services are working

---
+++ src/test/java/namingservice/NamingServiceTest.java
@@ -0,0 +1,50 @@
+package namingservice;
+
+import requestreplyapi.Registry.ExtendedEntry;
+import requestreplyapi.RequestReply.Requestor;
+import com.google.gson.Gson;
+import namingservice.replies.LocalizationReply;
+import namingservice.replies.RegistrationReply;
+import namingservice.requests.LocalizationRequest;
+import namingservice.requests.RegistrationRequest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NamingServiceTest {
+    private Gson gson;
+
+    @Before
+    public void setUp() throws Exception {
+//        NamingService namingService = new NamingService();
+//        namingService.startService();
+        gson = new Gson();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void registerRequestTest() {
+        Requestor r = new Requestor("SomeRequestor");
+        RegistrationRequest rr = new RegistrationRequest("NASDAQ", new ExtendedEntry("192.168.0.1", 1324, "StockMarket"));
+        String sresp = gson.toJson(rr);
+        byte[] send = sresp.getBytes();
+        byte[] resp = r.deliver_and_wait_feedback(NamingService.NAMING_SERVICE_ENTRY, send);
+        RegistrationReply reply = gson.fromJson(new String(resp), RegistrationReply.class);
+        System.out.println(gson.toJson(reply));
+    }
+
+    @Test
+    public void localizationRequestTest(){
+        registerRequestTest();
+        Requestor r = new Requestor("SomeRequestor");
+        LocalizationRequest lr = new LocalizationRequest("NASDAQ");
+        String sresp = gson.toJson(lr);
+        byte[] send = sresp.getBytes();
+        byte[] resp = r.deliver_and_wait_feedback(NamingService.NAMING_SERVICE_ENTRY, send);
+        LocalizationReply reply = gson.fromJson(new String(resp),LocalizationReply.class);
+        System.out.println(gson.toJson(reply));
+    }
+}
