8f8d161bcc1f15646544b4aff8700a3699515cb5
vendettavn <vladnemes5@gmail.com>
2018-05-09 23:16:18 +0300

    Added a logger class to be used while debugging in Visual Studio

---
+++ ObjectRequestBrokerCS/ObjectRequestBrokerCS/tests/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectRequestBrokerCS.tests
+{
+    class Log
+    {
+
+        public static void log(String message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
